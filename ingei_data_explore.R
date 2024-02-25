29b7fd0d412a54d6735deb94a935ab29dd82f9a0
Makenzie Kerneckel <mkenziek@sas.upenn.edu>
2024-02-24 22:13:51 -0500

    create data explore file

---
+++ ingei_data_explore.R
@@ -0,0 +1,6 @@
+
+library(foreign)
+
+a <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/defun21.dbf")
+
+b <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/CAPGPO.dbf")
