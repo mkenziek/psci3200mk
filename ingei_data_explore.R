b90ee8e7792ba55313dc9f8f12faae4ee570b771
Makenzie Kerneckel <mkenziek@sas.upenn.edu>
2024-02-29 17:34:57 -0500

    Update ingei explore

--- ingei_data_explore.R
+++ ingei_data_explore.R
@@ -1,11 +1,16 @@
 
 library(foreign)
+library(tidyverse)
 
-a <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/defun21.dbf")
+mx_mortality_21 <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/defun21.dbf")
 
-b <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/CAPGPO.dbf")
+load("/Users/kenziekmac/Downloads/clea_lc_20220908_r/clea_lc_20220908.RData")
 
+mx_low <- clea_lc_20220908 %>% 
+  filter(ctr_n == "Mexico",
+         yr == 2012) 
 
+acled <- read.csv("/Users/kenziekmac/Downloads/2020-01-01-2024-02-29-Mexico.csv")
 a$MUN_OCURR
 
 
