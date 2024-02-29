65e608906c0e4d3cd5b774f9bd19e3f57972fbe0
Makenzie Kerneckel <mkenziek@sas.upenn.edu>
2024-02-29 16:19:40 -0500

    Update ingei explore

--- ingei_data_explore.R
+++ ingei_data_explore.R
@@ -4,3 +4,9 @@ library(foreign)
 a <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/defun21.dbf")
 
 b <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/CAPGPO.dbf")
+
+
+a$MUN_OCURR
+
+
+max(as.numeric(as.character( a$MUN_OCURR )))
