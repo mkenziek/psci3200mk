36b29642c418f1c0e863b9b258bc0697ec8d4742
Makenzie Kerneckel <mkenziek@sas.upenn.edu>
2024-03-09 22:22:38 -0500

    Final Project Proposal Work

--- ingei_data_explore.R
+++ ingei_data_explore.R
@@ -1,17 +1,84 @@
 
 library(foreign)
 library(tidyverse)
+library(magrittr)
 
-mx_mortality_21 <- read.dbf("/Users/kenziekmac/Downloads/defunciones_base_datos_2021_dbf/defun21.dbf")
+onUser<-function(x){
+  user<-Sys.info()["user"]
+  onD<-grepl(x,user,ignore.case=TRUE)
+  return(onD)
+}
 
-load("/Users/kenziekmac/Downloads/clea_lc_20220908_r/clea_lc_20220908.RData")
-
-mx_low <- clea_lc_20220908 %>% 
-  filter(ctr_n == "Mexico",
-         yr == 2012) 
-
-acled <- read.csv("/Users/kenziekmac/Downloads/2020-01-01-2024-02-29-Mexico.csv")
-a$MUN_OCURR
+if(onUser("kenziekmac")){
+  setwd("~/Users/kenziekmac/Dropbox/Mac/Documents/GitHub/psci3200mk")
+  figures<-"~/Users/kenziekmac/Dropbox/Mac/Documents/GitHub/psci3200mk/Figures"
+  tables<-"~/Users/kenziekmac/Dropbox/Mac/Documents/GitHub/psci3200mk/Tables"
+}
 
 
-max(as.numeric(as.character( a$MUN_OCURR )))
+
+election_returns <- read.csv("Data/aymu1970-on.coalSplit.csv")
+
+homicide_rates <- readr::read_delim("Data/mexico-muni-month-homicide-rates-2000-2021.csv", delim = "|")
+
+# have leading zeros so all municipal level identifiers are 5 digits
+
+head(unique(homicide_rates$ent_mun))
+
+# does not have leading zeros 
+
+head(unique(election_returns$inegi))
+
+# let's add leading zeros to the election_returns data 
+
+# Function to add leading zeros
+
+add_leading_zeros <- function(x) {
+  ifelse(nchar(x) == 4, paste0("0", x), as.character(x))
+}
+
+# Apply the function to the column
+
+election_returns$inegi <- as.numeric(election_returns$inegi)
+
+election_returns$inegi <- sapply(election_returns$inegi, add_leading_zeros) 
+
+
+election_returns %<>% rename( "ent_mun" = "inegi")
+  
+# Check for municipal codes present in df1 but not in df2
+
+# these are missing election returns 
+
+records_only_in_df1 <- anti_join(election_returns, homicide_rates, by = "ent_mun")
+
+records_only_in_df1 
+
+
+# Check for municipal codes present in df2 but not in df1
+
+# these are missing homicide rates
+
+records_only_in_df2 <- anti_join(homicide_rates, election_returns, by = "ent_mun")
+          
+
+# next steps
+
+# drop rows from anti_join
+
+# drop ent_mun NAs
+
+
+# calc. pan/pri vote variable 
+
+# pick columns you need
+
+# pick time frame 
+
+# merge 
+
+
+
+
+
+
